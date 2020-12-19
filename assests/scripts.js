83b9b40e7cd1612125909dabeba7088db83ba777
Claire Bohlen <clairebohlen@gmail.com>
2020-12-19 11:05:16 -0700

    finish JS

--- assests/scripts.js
+++ assests/scripts.js
@@ -1,3 +1,16 @@
 const open = document.getElementById('open')
 const close = document.getElementById('close')
 const container = document.querySelector('.container')
+
+
+//add event listener for open and change the class name to show nav
+
+open.addEventListener('click', () => {
+    container.classList.add('show-nav')
+})
+
+//add event listener for close and remove the class name of show nav
+
+close.addEventListener('click', ()=>{
+    container.classList.remove('show-nav')
+})
